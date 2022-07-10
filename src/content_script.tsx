f65f4b972f52b30e40232866775a4236d73d5cd0
Lauro Figueiredo <lauro.figueiredo@avenuecode.com>
2022-07-09 21:06:28 -0300

    fix loop, set color with priority

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -55,21 +55,27 @@ function rgbStringToObject(rgbString: string): RGB {
   return obj;
 }
 
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg.color) {
-    const all = document.getElementsByTagName('*');
+const setColor = (element: HTMLElement, color: string): void => {
+  const elementColor = getComputedStyle(element).color;
 
-    for (let i = 0, max = all.length; i < max; i + 1) {
-      const element = all[i] as HTMLElement;
-      const elementColor = getComputedStyle(element).color;
-      const isHex = elementColor[0] === '#';
-      const elementColorRgb = isHex ? hexToRgb(elementColor) : rgbStringToObject(elementColor);
+  const isHex = elementColor[0] === '#';
+  const elementColorRgb = isHex ? hexToRgb(elementColor) : rgbStringToObject(elementColor);
 
-      if (itsTooGrey(elementColorRgb)) {
-        element.style.color = msg.color;
-      }
+  if (itsTooGrey(elementColorRgb)) {
+    element.style.setProperty('color', color, 'important');
+  }
+};
+
+chrome.runtime.onMessage.addListener(({ color }, sender, sendResponse) => {
+  if (color) {
+    const all = document.getElementsByTagName('*') as any as HTMLElement[];
+
+    for (let i = 0; i < all.length; i += 1) {
+      const element = all[i];
+      setColor(element, color);
     }
-    sendResponse(`Change color to ${msg.color}`);
+
+    sendResponse(`Change color to ${color}`);
   } else {
     sendResponse('Color message is none.');
   }
